c6f00b423aaeb4a4f6fdf8bbfed827efd3042545
sampaixd <samuelbroman03@gmail.com>
2021-10-19 15:21:02 +0200

    Added a system for creating, placing and running into enemies. Will probably work on either combat or a loot system next

--- HeroGame/Program.cs
+++ HeroGame/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading;
 
 namespace HeroGame
 {
@@ -12,6 +13,7 @@ namespace HeroGame
         static void Main(string[] args)
         {
             List<Castle> castles = new List<Castle>();
+            List<Enemy> enemies = new List<Enemy>();
             //all equipment
             Sword sword1 = new Sword(10, rarity.common, set.none);
             Sword sword2 = new Sword(20, rarity.common, set.none);
@@ -53,7 +55,7 @@ namespace HeroGame
                 Console.WriteLine("Please enter input");
                 input = Console.ReadLine();
                 Console.Clear();
-                switch (input)
+                switch (input)  //used for testing
                 {
                     case "info":
                         Player.ViewStats();
@@ -77,14 +79,26 @@ namespace HeroGame
                         Player.ChangeHelmet(helmet10);
                         break;
                     case "lvl":
-                        Player.LVLUp(0);
+                        Player.LVLUp();
                         break;
                     case "pick":
                         GenerateCastles(castles, Player);
                         int test = PickCastle(castles, Player);
                         break;
                     case "tower":
-                        InTower(1, 6);
+                        InTower(1, 6, enemies, Player);
+                        break;
+                    case "enemies":
+                        GenerateEnemies(6, 1, enemies);
+                        int number = 1;
+                        foreach (var i in enemies)
+                        {
+                            Console.Write(number + ": ");
+                            i.Info();
+                            Console.WriteLine("\n");
+                            number++;
+                        }
+                        Console.ReadLine();
                         break;
 
                         //Sword sword = EquipmentManager.GetWeapons(EquipmentManager.FireSwords);   //tages ide för att hämta vapen
@@ -93,58 +107,57 @@ namespace HeroGame
             }
 
         }
-        static void GenerateCastles(List<Castle> castles, Hero Player)
+        static void GenerateCastles(List<Castle> castles, Hero Player)  //method that generates which castles can be selected
         {
-            castles.Clear();
+            castles.Clear();    //clears the list to make sure that the castles selection pool is always correct based on player lvl
             Random rng = new Random();
             int herolvl = Player.LVL;
             int castlelvl;
             size castlesize;
             for (int i = 0; i < 3; i++)
             {
-                castlelvl = rng.Next(herolvl, herolvl + 3);
-                castlesize = (size)rng.Next(2);
-                castles.Add(new Castle(castlelvl, castlesize));
+                castlelvl = rng.Next(herolvl, herolvl + 3); //randoms a lvl between the heros current lvl up to 2 lvls above
+                castlesize = (size)rng.Next(2);             //randoms a size, this is relevant when deciding the ammount of towers
+                castles.Add(new Castle(castlelvl, castlesize));//adds the castle to the list, 
             }
         }
 
         static int PickCastle(List<Castle> castles, Hero Player)
         {
             int selectedcastle = 0; //defines what option you currently have selected, as well as the "reroll" option
-            bool selectloop = true;
+            bool selectloop = true; //bool that keeps the menu open until you select a valid option 
             while (selectloop)
             {
-                for (int i = 0; i < 4; i++)
+                for (int i = 0; i < 4; i++)     //writes all 4 options, as well as changing the color depending if the option is selected or not
                 {
                     Console.Write("       ");
                     if (selectedcastle == i)
                     {
-                        Console.BackgroundColor = ConsoleColor.White;
+                        Console.BackgroundColor = ConsoleColor.White;   //changes background color and text color
                         Console.ForegroundColor = ConsoleColor.Black;
                     }
                     if (i != 3)
-                    { Console.Write("Castle " + (i + 1)); }
+                    { Console.Write("Castle " + (i + 1)); } //writes the current castle if it isn't the last loop
                     else
-                    { Console.Write("Reroll"); }
-                    Console.BackgroundColor = ConsoleColor.Black;
+                    { Console.Write("Reroll"); }    //otherwise writes the reroll option
+                    Console.BackgroundColor = ConsoleColor.Black;   //reverts background color and text color to its original state
                     Console.ForegroundColor = ConsoleColor.White;
                 }
                 Console.WriteLine();
-                Console.Write("       LVL: " + castles[0].Castlelvl);
+                Console.Write("       LVL: " + castles[0].Castlelvl);   //writes the levels of all castles
                 for (int i = 1; i < 3; i++)
                 {
-                    if (castles[i - 1].Castlelvl > 9)
-                    { Console.Write("        "); }
+                    if (castles[i - 1].Castlelvl > 9)   //different ammount of blank spaces depending on how many numbers, in order to make
+                    { Console.Write("        "); }      //it look better
                     else
                     { Console.Write("         "); }
                     Console.Write("LVL: " + castles[i].Castlelvl);
                 }
                 Console.WriteLine();
-                // + "         LVL: " + castles[1].Castlelvl + "         LVL: " + castles[2].Castlelvl
                 Console.Write("       Size: " + castles[0].Castlesize);
                 for (int i = 1; i < 3; i++)
                 {
-                    if (castles[i - 1].Castlesize == size.small)
+                    if (castles[i - 1].Castlesize == size.small)    //same as with the levels, adding blank spaces for it to look better
                     { Console.Write("    "); }
                     else if (castles[i - 1].Castlesize == size.medium)
                     { Console.Write("   "); }
@@ -153,10 +166,10 @@ namespace HeroGame
                     Console.Write("Size: " + castles[i].Castlesize);
                 }
                 Console.WriteLine();
-                ConsoleKeyInfo button = Console.ReadKey();
+                ConsoleKeyInfo button = Console.ReadKey();  //reads the input key
                 switch (button.Key)
                 {
-                    case ConsoleKey.D:
+                    case ConsoleKey.D:  //changes the currently selected option based on the user input
                     case ConsoleKey.RightArrow:
                         if (selectedcastle == 3)
                         { selectedcastle = 0; }
@@ -171,11 +184,11 @@ namespace HeroGame
                         { selectedcastle--; }
                         break;
                     case ConsoleKey.Enter:
-                        if (selectedcastle == 3)
+                        if (selectedcastle == 3)    //if the currently selected option is "reroll", reroll the current castles
                         {
                             GenerateCastles(castles, Player);
                         }
-                        else
+                        else    //otherwise break the loop and return what castle was selected
                         { selectloop = false; }
                         break;
                 }
@@ -183,20 +196,21 @@ namespace HeroGame
             }
             return selectedcastle;
         }
-        static void InTower(int towerlvl, int rooms)
+        static void InTower(int towerlvl, int rooms, List<Enemy> enemies, Hero Player)
         {
-            bool programloop = true;
-            int currentroom = rooms - 1;
+            bool programloop = true;    //keeps the program alive
+            int currentroom = rooms - 1;    //looks at what room you are currently in
             int currentrooms = rooms;   //int to only display the current rooms, as updating what rooms were displayed all the time made it look as you made noprogression
-            int currentroompos = 0;
+            int currentroompos = 0; //where you are in the current room
+            int enemynumber = 0;
             while (programloop)
             {
-                for (int i = currentrooms - 2; i < currentrooms; i++)
+                for (int i = currentrooms - 2; i < currentrooms; i++)   //the loop only draws 2 rooms to make it look better
                 {
-                    InTowerBlankSpaces(currentrooms, rooms);
-                    if (currentroom == i && currentroompos == 6)
-                    { Console.WriteLine("     --#--"); }
-                    else
+                    InTowerBlankSpaces(currentrooms, rooms);    //adds blank spaces if you have traversed the tower enough, to give
+                    if (currentroom == i && currentroompos == 6)//the user a better sense of progression
+                    { Console.WriteLine("     --#--"); }        //writes a "#" as the players current position, if the player is not at that position
+                    else                                        //it instead writes a blank space where the player would be
                     { Console.WriteLine("     -- --"); }
                     InTowerBlankSpaces(currentrooms, rooms);
                     if (currentroom == i && currentroompos == 5)
@@ -229,29 +243,43 @@ namespace HeroGame
                     else
                     { Console.WriteLine("      | |"); }
                 }
-                Console.WriteLine("currentroompos: " + currentroompos + "\ncurrentroom: " + currentroom + "\ncurrentrooms: " + currentrooms);
-                ConsoleKeyInfo button = Console.ReadKey();
+                enemynumber = 0;
+                foreach (var i in enemies)
+                {
+                    int enemyroom = i.Room;
+                    int enemyroompos = i.Roompos;
+                    if (enemyroom == currentroom && enemyroompos == currentroompos)
+                    {
+                        Console.WriteLine("Enemy located...");
+                        Thread.Sleep(2000);
+                        Combat(enemies, enemynumber, Player);
+                        break;
+                    }
+                    enemynumber++;
+                }
+                //Console.WriteLine("currentroompos: " + currentroompos + "\ncurrentroom: " + currentroom + "\ncurrentrooms: " + currentrooms);   //used for testing
+                ConsoleKeyInfo button = Console.ReadKey();  //reads user input
                 switch (button.Key)
                 {
                     case ConsoleKey.W:
-                    case ConsoleKey.UpArrow:
-                        if (currentroompos == 6 && currentroom == 0)
+                    case ConsoleKey.UpArrow:    //traverses the player up or down based on player input
+                        if (currentroompos == 6 && currentroom == 0)    //if you have reached the end of the tower the loop stops
                         { programloop = false; }
-                        else if (currentroompos >= 6)
+                        else if (currentroompos >= 6)   //if you have not reached the end of the tower but instead reached the end of a room
                         {
-                            if (currentrooms >= currentroom + 2)
-                            { currentrooms -= 2; }
-                            currentroom--;
-                            currentroompos = 0;
+                            if (currentrooms >= currentroom + 2)    //checks if you have traversed 2 rooms 
+                            { currentrooms -= 2; }                  //if you have, change the currentrooms int to display the correct rooms in the for loop
+                            currentroom--;                          //changes your current room
+                            currentroompos = 0;                     //resets the current room pos
                         }
                         else
                         { currentroompos++; }
                         break;
                     case ConsoleKey.S:
                     case ConsoleKey.DownArrow:
-                        if (currentroompos == 0 && currentroom == (rooms - 1))
+                        if (currentroompos == 0 && currentroom == (rooms - 1))  //does nothing if you try to backtrack in the beginning
                         { }
-                        else if (currentroompos == 0)
+                        else if (currentroompos == 0)   //does the same things as moving forward, only changed in order to work the other way around
                         {
                             if (currentrooms <= currentroom + 1)
                             { currentrooms += 2; }
@@ -273,6 +301,74 @@ namespace HeroGame
                 Console.Write("          ");
             }
         }
+
+        static void GenerateEnemies(int rooms, int towerlvl, List<Enemy> enemies)   //generates enemies for the tower based on the ammount of rooms
+        {
+            enemies.Clear();    //resets the list when you need to make new enemies
+            Random rng = new Random();
+            int enemycount = rng.Next(rooms, (rooms * 2) + 1);  //creates a random number between the ammount of room and twice the ammount of rooms
+            //int enemiesleft = enemies.Count;
+            for (int i = 0; i < rooms; i++)
+            {                                               //depending on how many enemies have aleady been placed, the max/min ammount of enemies
+                int roomenemies = enemycount / (rooms - i);//will change accordingly
+                int roomenemiescount = rng.Next(roomenemies - 1, roomenemies + 1);
+                int enemiescreated = 0;
+                for (int x = 0; x < 6; x++)
+                {
+                    if (6 - x > roomenemiescount)
+                    {
+                        int createenemy = rng.Next(2);
+                        if (createenemy == 1)
+                        {
+                            enemies.Add(new Enemy(towerlvl, i, x));
+                            enemiescreated++;
+                        }
+                    }
+                    else
+                    {
+                        enemies.Add(new Enemy(towerlvl, i, x));
+                        enemiescreated++;
+                    }
+                    if (enemiescreated >= roomenemiescount)
+                    { break; }
+                }
+                enemycount -= roomenemiescount;
+            }
+
+            /*for (int i = 0; i < enemycount; i++)                //for example: if the ammount of rooms is 6, create a number between 6 and 12 (6*2)
+            {
+                enemies.Add(new Enemy(towerlvl));
+            }*/
+        }
+
+        static void EnemyPlacement(List<Enemy> enemies, int rooms)
+        {
+            Random rng = new Random();
+            int enemiesleft = enemies.Count;
+            for (int i = 0; i < rooms; i++)
+            {                                               //depending on how many enemies have aleady been placed, the max/min ammount of enemies
+                int roomenemies = enemiesleft / (rooms - i);//will change accordingly
+                int roomenemiescount = rng.Next(roomenemies - 1, roomenemies + 1);
+                for (int x = 0; x < 6; x++)
+                {
+                    int createenemy = rng.Next(2);
+                    if (createenemy == 1)
+                    { }
+                }
+                enemiesleft -= roomenemiescount;
+            }
+        }
+
+        static void Combat(List<Enemy> enemies, int enemynumber, Hero Player)
+        {
+            Console.Clear();
+            Console.WriteLine("Press any key to kill enemy");
+            Console.ReadKey();
+            int ganinedxp = enemies[enemynumber].LVL * 5;
+            Player.Killedenemy(ganinedxp);
+            enemies.RemoveAt(enemynumber);
+        }
+
 
     }
 }
